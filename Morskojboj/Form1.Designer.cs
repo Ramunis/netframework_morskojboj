cff6b767d36a13b558bc75f2dfd4167c6d398442
Ramunis <seva.m@live.com>
2022-05-03 18:09:44 +1000

    2D Game v1.0

---
+++ Morskojboj/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MorskojBoj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 20);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Подключиться как";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(320, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 17);
+            this.radioButton1.TabIndex = 18;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Сервер";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(388, 9);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 19;
+            this.radioButton2.Text = "Клиент";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(824, 39);
+            this.panel1.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "IP адрес:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Image = global::Morskojboj.Properties.Resources.sea;
+            this.label8.Location = new System.Drawing.Point(500, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(257, 33);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Вражеское Поле";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Image = global::Morskojboj.Properties.Resources.sea;
+            this.label7.Location = new System.Drawing.Point(114, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 33);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Моё Поле";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 479);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(824, 22);
+            this.statusStrip1.TabIndex = 30;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Счёт: ";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "                                     ";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel3.Text = "Статус:";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(76, 17);
+            this.toolStripStatusLabel4.Text = "                       ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.BackgroundImage = global::Morskojboj.Properties.Resources.sea;
+            this.ClientSize = new System.Drawing.Size(824, 501);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Морской Бой";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
+
---
+++ Morskojboj/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace MorskojBoj
+{
+    public partial class Form1 : Form
+    {
+        //массивы игровых полей
+        Random rnd = new Random();  //рандом генератор для генерации кораблей
+        int[,] mypole = new int[12, 12];  //матрица моего поля
+        int[,] enemypole = new int[12, 12];  //матрица вражьего поля
+        int[,] atacking = new int[12, 12];   //матрица для аттаки 
+
+        bool step = false;   //определяет есть ход или нет
+        string mypolesend;     //сюда матрица будет записываться в строку для дальнейшей отправки
+        Graphics gr;         // холст для рисования
+        int balls = 0;      //счёт игры
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            gr = this.CreateGraphics();
+
+            timer1.Interval = 10;           //интервал таймера
+            timer1.Tick += timer1_Tick;     //инкрементация таймера 
+
+            ipPoint = new IPEndPoint(IPAddress.Parse(textBox1.Text),port1);  // отвечает за подключения к другому компьютеру 
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);  //определяет протокол TCP в сокете
+        }
+
+        //Сетевые сокеты
+        public IPEndPoint ipPoint;
+        public Socket listenSocket;
+        public Socket handler;
+
+        public int port1 = 3300;  //адрес Порта
+
+        //Функция для установления соединения между сервером и клиентом
+        public void Connect()
+        {
+            if (radioButton1.Checked == true)  //играем за сервер
+            {
+                listenSocket.Bind(ipPoint); //Связывает объект Socket с локальной конечной точкой.
+                listenSocket.Listen(10);      //Ожидание принятия соединения          
+                handler = listenSocket.Accept(); //зависаем до соединения клиента
+
+
+                step = false;  //шага нет 
+                atack(0, 0, handler);    //вызов главной функции для присваения в неё сокета handler - точнее зависания до ожидания клиента
+            }
+            else  //играем за клиент 
+            {
+                listenSocket.Connect(ipPoint);  //подключение к хосту
+                step = true;                    //ход есть
+            }
+            connecting = true;  //соединение есть
+        }
+
+
+        byte[] data = new byte[256];  // массив байт для передачи
+        StringBuilder builder = new StringBuilder(); // изменяемая строка символов
+        Socket SockettoTimer;  //сокет для таймера
+        public void atack(int X, int Y, Socket SendSocket)  //функция отправки и приёма координат и их анализа 
+        {
+            SockettoTimer = SendSocket;
+            if (step == false)  //не ходим
+            {
+                toolStripStatusLabel4.Text = "Ожидание";
+                int bytes = SendSocket.Receive(data, data.Length, 0);                   //зависаем, пока не придут данные от клиента
+                X = Convert.ToInt32(Encoding.Unicode.GetString(data, 0, bytes));  // добавляем в конец строки Builder полученные данные
+                bytes = SendSocket.Receive(data, data.Length, 0);              // данные data записываются в байт, после получения сервер развисает
+                Y = Convert.ToInt32(Encoding.Unicode.GetString(data, 0, bytes));  // добавляем в конец строки Builder полученные данные
+
+
+                if (mypole[X + 1, Y + 1] == 1)   //если в ячейке массива по полученым координатам, есть кораблик, то отправляем единицу
+                {
+                    data = Encoding.Unicode.GetBytes("1"); //кодирование сообщения о победе
+                    SendSocket.Send(data); //отправка сообщения о победе
+        
+                    mypole[X + 1, Y + 1] = 2;   //Заменяем в поле того, по кому стреляли, кораблик на подбитый кораблик
+                    timer1.Start();                    
+                }
+                else
+                {                   
+                    step = true;        //надо ходить       
+                    toolStripStatusLabel4.Text = "Надо ходить";
+                    data = Encoding.Unicode.GetBytes("0");   //кодируем сообщение о проигрыше 
+                    SendSocket.Send(data);     //отправлем сообщение о проигрыше                 
+                    mypole[X + 1, Y + 1] = 3;  //Заменяем ячейку, на стреляную  
+                }
+                MyPoleGen();     // генерация моего поля        
+            }
+            else
+            {
+                // отправка коорд
+                data = Encoding.Unicode.GetBytes(X.ToString());  
+                SendSocket.Send(data);
+
+                data = Encoding.Unicode.GetBytes(Y.ToString());   
+                SendSocket.Send(data);
+      
+                int bytes = SendSocket.Receive(data, data.Length, 0);  //принять дату 
+                if (Encoding.Unicode.GetString(data, 0, bytes) == "1")  //декодировать дату 
+                {                  
+                    atacking[X + 1, Y + 1] = 2;     //попал задаем режим 2             
+                }
+                else
+                {                   
+                    atacking[X + 1, Y + 1] = 3;   //не попал задаем режим 3
+
+                    step = false;  //не ходим
+                    timer1.Start();
+                }
+                MyPoleGen();     //генерация моего поля
+                EnemyPoleGen();   //генерация вражьего поля
+            }          
+        }
+
+        public void Clean()  //очистить матрицу
+        {
+            for (int z = 0; z <= 11; z++)
+            {
+                for (int q = 0; q <= 11; q++)
+                {
+                    mypole[z, q] = 0;
+                }
+            }
+
+            for (int z = 0; z <= 11; z++)
+            {
+                for (int q = 0; q <= 11; q++)
+                {
+                    atacking[z, q] = 0;
+                }
+            }
+        }
+
+        public void Gen()  //герерация кораблей
+        {
+            double napravl; //равно 0 или 1, отвечает за направление по горизонтали или вертикали
+            int x, y;  //координаты первой точки корабля
+            int x_end, y_end;
+            for (int i = 4; i >= 1; i--)
+            {
+                for (int j = 0; j < 5 - i; j++)
+                {
+                    x = rnd.Next(1, 11);
+                    y = rnd.Next(1, 11);
+                    napravl = rnd.Next(0, 2);
+                    if (napravl == 0 && y + i - 1 > 10)
+                    {
+                        j--;
+                        continue;
+                    }
+                    if (napravl == 1 && x + i - 1 > 10)
+                    {
+                        j--;
+                        continue;
+                    }
+                    if (napravl == 0)
+                    {
+                        x_end = x + 1;
+                        y_end = y + i;
+                    }
+                    else
+                    {
+                        x_end = x + i;
+                        y_end = y + 1;
+                    }
+                    for (int z = x - 1; z <= x_end; z++)
+                    {
+                        for (int q = y - 1; q <= y_end; q++)
+                        {
+                            if (mypole[z, q] != 0)
+                            {
+                                z = 12;
+                                y_end = -1;
+                                break;
+                            }
+                        }
+                    }
+                    if (y_end == -1)
+                    {
+                        j--;
+                        continue;
+                    }
+                    for (int z = x; z <= x_end - 1; z++)
+                    {
+                        for (int q = y; q <= y_end - 1; q++)
+                        {
+                            mypole[z, q] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Отрисовываем корабли игрока
+        public void MyPoleGen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {                 
+                    if (mypole[i + 1, j + 1] == 0)    //пустота
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 5 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35); //море
+                    
+                    if (mypole[i + 1, j + 1] == 1)     //корабль
+                        gr.FillRectangle(new SolidBrush(Color.BlueViolet), 5 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35);  //корабль
+                 
+                    if (mypole[i + 1, j + 1] == 2)        //попал
+                    {
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 5 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35);  //море
+                        gr.DrawString("X", new System.Drawing.Font("Arial", (float)22), new SolidBrush(Color.Black), 12 + j * 35 + 2 * j, 82 + i * 35 + 2 * i); //крест
+                       
+                    }
+                   
+                    if (mypole[i + 1, j + 1] == 3)    //не попал  
+                    {
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 5 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35); //море
+                        gr.FillEllipse(new SolidBrush(Color.Black), 457 + j * 35 + 2 * j, 82 + i * 35 + 2 * i, 20, 20);  //мина у меня на поле для стрельбы
+                        gr.FillEllipse(new SolidBrush(Color.Black), 12 + j * 35 + 2 * j, 82 + i * 35 + 2 * i,20,20);    //мина на вражеском поле
+                    }
+                }
+            }
+        }
+
+        //Отрисовываем корабли противника (куда стрелял игрок)
+        public void EnemyPoleGen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {                                  
+                    if (atacking[i + 1, j + 1] == 0)      //пустота
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 450 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35); //море
+                    
+                    if (atacking[i + 1, j + 1] == 2)      //попал
+                    {
+                        balls = balls+1;
+                        toolStripStatusLabel2.Text = Convert.ToString(balls);   //счёт игры                   
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 450 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35); //море
+                        gr.DrawString("X", new System.Drawing.Font("Arial", (float)22), new SolidBrush(Color.Black), 450 + j * 35 + 2 * j, 75 + i * 35 + 2 * i); //крест на вражьем поле
+                        gr.DrawString("X", new System.Drawing.Font("Arial", (float)22), new SolidBrush(Color.Black), 12 + j * 35 + 2 * j, 82 + i * 35 + 2 * i);  //крест у меня на поле для стрельбы
+                    }
+                   
+                    if (atacking[i + 1, j + 1] == 3)      //не попал
+                    {
+                        toolStripStatusLabel2.Text = Convert.ToString(balls);
+                        gr.FillRectangle(new SolidBrush(Color.DodgerBlue), 450 + j * 35 + 2 * j, 75 + i * 35 + 2 * i, 35, 35);  //море
+                        gr.FillEllipse(new SolidBrush(Color.Black), 457 + j * 35 + 2 * j, 82 + i * 35 + 2 * i, 20, 20);  //мина у меня на поле для стрельбы
+                        gr.FillEllipse(new SolidBrush(Color.Black), 12 + j * 35 + 2 * j, 82 + i * 35 + 2 * i,20,20);     //мина на вражеском поле
+                    }
+                }
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) //функция срабатывет при обновлении формы
+        {        
+            MyPoleGen();          //генерация моего поля
+            EnemyPoleGen();         //генерация вражьего поля
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clean(); //чистка дисплея
+            Gen();   //общая генерация
+            MyPoleGen();    // генерация моего поля
+            EnemyPoleGen();  // генерация вражьего поля
+
+            //массив сгенерированных кораблей, превращается в строку, для отправки другому игроку
+            mypolesend = null;
+            for (int z = 0; z <= 11; z++)
+            {
+                for (int q = 0; q <= 11; q++)
+                {
+                    mypolesend = mypolesend + mypole[z, q];  //формирование строки
+                }
+            }
+            Connect();
+        }
+
+        public bool connecting;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (connecting == true)  //если соединение установлена 
+            { 
+                if (step == true)  //если хожу 
+                { 
+                    int x = 1000000; //infinity
+                    int y = 1000000; //
+                    if (e.X > 450 && e.Y < 475 && e.X < 819 && e.Y > 75)   //самая важная часть - проверяет поле
+                    {                                                      //
+                        x = ((e.X - 450) / 37);    // превращает координваты в матрицы                        
+                        y = (e.Y - 75) / 37;                               //
+                       
+                        if (radioButton1.Checked == true) //если сервер играет
+                            atack(y, x, handler);    //отправить только что нажатые координаты
+                        else                        //если клиент играет
+                            atack(y, x, listenSocket);   //отправить только что нажатые координаты
+                    }
+                }
+            }
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            atack(0, 0, SockettoTimer);  //запуск функции отправки и анализа координат в таймер
+        }
+        
+    }
+}
